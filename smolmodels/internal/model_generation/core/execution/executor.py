6d29bbf68c3ebc4d01d175f897c11485e42ffcef
marcellodebernardi <marcello.logins@outlook.com>
2025-01-15 19:06:00 +0000

    Add base skeleton for model generation package

---
+++ smolmodels/internal/model_generation/core/execution/executor.py
@@ -0,0 +1,5 @@
+import abc
+
+
+class Executor(abc.ABC):
+    pass
---
+++ smolmodels/internal/model_generation/generators.py
@@ -0,0 +1,10 @@
+class Generator:
+    pass
+
+
+class WorkflowBasedGenerator(Generator):
+    pass
+
+
+class MultiAgentGenerator(Generator):
+    pass
