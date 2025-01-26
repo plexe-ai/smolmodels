e12b818b6a725449cea61199243219d117179b5a
marcellodebernardi <marcello.logins@outlook.com>
2025-01-26 17:03:12 +0000

    Add docstrings to provider abc

--- smolmodels/internal/common/providers/provider.py
+++ smolmodels/internal/common/providers/provider.py
@@ -2,6 +2,18 @@ import abc
 
 
 class Provider(abc.ABC):
+    """
+    Abstract base class for LLM providers.
+    """
+
     @abc.abstractmethod
     def query(self, system_message: str, user_message: str, response_format=None) -> str:
+        """
+        Abstract method to query the provider.
+
+        :param [str] system_message: The system message to send to the provider.
+        :param [str] user_message: The user message to send to the provider.
+        :param [str] response_format: The format of the response.
+        :return [str]: The response from the provider.
+        """
         pass
