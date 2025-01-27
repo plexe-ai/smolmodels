6f7d5a678d2961d9ac393112350de46755a94509
marcellodebernardi <marcello.logins@outlook.com>
2025-01-27 18:31:08 +0000

    Add logging to utils

--- smolmodels/internal/models/utils.py
+++ smolmodels/internal/models/utils.py
@@ -1,12 +1,16 @@
 import json
+import logging
+import textwrap
 
 from smolmodels.internal.models.entities.node import Node
 from smolmodels.internal.models.execution.executor import Executor
 
+logger = logging.getLogger(__name__)
+
 
 def join_problem_statement(intent: str, input_schema: dict, output_schema: dict, constraints, directives) -> str:
     """Join the problem statement into a single string."""
-    return (
+    problem_statement: str = (
         "# Problem Statement"
         "\n\n"
         f"{intent}"
@@ -27,14 +31,17 @@ def join_problem_statement(intent: str, input_schema: dict, output_schema: dict,
         "\n\n"
         f"{json.dumps(directives, indent=4, default=str)}"
     )
+    logger.debug(f"Joined user inputs into problem statement: {textwrap.shorten(problem_statement, 40)}")
+    return problem_statement
 
 
 def execute_node(node: Node, executor: Executor) -> None:
     """
     Execute the training code for the given node using the executor.
     """
-
+    logger.debug(f"Executing node {node} using executor {executor}")
     result = executor.run()
+    logger.debug(f"Execution result: {result}")
     node.execution_time = result.exec_time
     node.execution_stdout = result.term_out
     node.execution_stderr = result.exc_stack or None
@@ -43,3 +50,4 @@ def execute_node(node: Node, executor: Executor) -> None:
     node.model_artifacts = result.model_artifacts
     node.analysis = result.analysis
     node.performance = result.performance
+    logger.debug(f"Unpacked execution results into node: {node}")
