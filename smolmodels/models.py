4e81351c56b7e4d79795af31ad35408d9c09642f
marcellodebernardi <marcello.logins@outlook.com>
2025-01-29 22:13:51 +0000

    Add file cleanup in saving/loading exceptions

--- smolmodels/models.py
+++ smolmodels/models.py
@@ -343,7 +343,9 @@ def save_model(model: Model, path: str) -> None:
             tar.addfile(model_data_tarinfo, model_data_bytes)
 
     except Exception as e:
-        logger.error(f"Error saving model: {e}")
+        logger.error(f"Error saving model, cleaning up tarfile: {e}")
+        if Path(path).exists():
+            Path(path).unlink()
         raise e
     finally:
         # Cleanup model cache directory
@@ -426,7 +428,9 @@ def load_model(path: str) -> Model:
         return model
 
     except Exception as e:
-        logger.error(f"Error loading model: {e}")
+        logger.error(f"Error loading model, cleaning up model files: {e}")
+        if model is not None and model.files_path.exists():
+            shutil.rmtree(model.files_path)
         raise e
 
     finally:
